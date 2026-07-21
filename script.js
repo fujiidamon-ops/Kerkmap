a7e15b118541f538201957af9dfd1d658b7e3fde
fujiidamon-ops <fujiidamon@gmail.com>
2026-07-21 20:30:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
-// あなたのAccess Token
-mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhY2hhMzI4IiwiYSI6ImNtcWFnYTcyNDA2Z2MycnBwNWJ2Z2pucXkifQ.Z7dIsGQl4CeGw4HkYKh6qg';
+new mapboxgl.Popup({
+    closeButton: true,
+    closeOnClick: true,
+    maxWidth: "320px"
+})
+.setLngLat(e.lngLat)
+.setHTML(`
+<div class="popup">
 
-const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/chacha328/cmr7bti6i004a01rd8c7eb7rk',
-    center: [5.3, 52.1],
-    zoom: 7
-});
+    <h2>${p.Name}</h2>
 
-map.addControl(new mapboxgl.NavigationControl());
-map.addControl(new mapboxgl.FullscreenControl());
+    <p class="jp">${p.JP}</p>
 
-map.addControl(new mapboxgl.GeolocateControl({
-    positionOptions: {
-        enableHighAccuracy: true
-    },
-    trackUserLocation: true
-}));
+    <hr>
 
-// ↓ここから追加
-map.on('load', () => {
+    <table>
 
-    // クリック
-    map.on('click', 'fd42479d8f62b097d0aa (1)', (e) => {
+        <tr>
+            <th>City</th>
+            <td>${p.City}</td>
+        </tr>
 
-        const p = e.features[0].properties;
+        <tr>
+            <th>Architect</th>
+            <td>${p.Architect}</td>
+        </tr>
 
-        new mapboxgl.Popup()
-            .setLngLat(e.lngLat)
-            .setHTML(`
-                <h3>${p.Name}</h3>
-            `)
-            .addTo(map);
-    });
+        <tr>
+            <th>Year</th>
+            <td>${p.Year}</td>
+        </tr>
 
-    // カーソル変更
-    map.on('mouseenter', 'fd42479d8f62b097d0aa (1)', () => {
-        map.getCanvas().style.cursor = 'pointer';
-    });
+        <tr>
+            <th>Style</th>
+            <td>${p.Style}</td>
+        </tr>
 
-    map.on('mouseleave', 'fd42479d8f62b097d0aa(1)', () => {
-        map.getCanvas().style.cursor = '';
-    });
+    </table>
 
-});
+    <a href="${p.Link}" target="_blank" class="button">
+        Read More
+    </a>
+
+</div>
+`)
+.addTo(map);
