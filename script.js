8babe730283bf325c3c6faf5c609ca5a4ececd27
fujiidamon-ops <fujiidamon@gmail.com>
2026-07-21 20:21:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,46 @@
 // あなたのAccess Token
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhY2hhMzI4IiwiYSI6ImNtcWFnYTcyNDA2Z2MycnBwNWJ2Z2pucXkifQ.Z7dIsGQl4CeGw4HkYKh6qg';
 
-// 地図を作成
 const map = new mapboxgl.Map({
-
     container: 'map',
-
-    // あなたのStyle URL
     style: 'mapbox://styles/chacha328/cmr7bti6i004a01rd8c7eb7rk',
-
-    center: [5.3,52.1],   // オランダ
-
+    center: [5.3, 52.1],
     zoom: 7
-
 });
 
-// 拡大縮小ボタン
 map.addControl(new mapboxgl.NavigationControl());
-
-// 全画面ボタン
 map.addControl(new mapboxgl.FullscreenControl());
 
-// 現在地ボタン
 map.addControl(new mapboxgl.GeolocateControl({
-    positionOptions:{
-        enableHighAccuracy:true
+    positionOptions: {
+        enableHighAccuracy: true
     },
-    trackUserLocation:true
+    trackUserLocation: true
 }));
-// 教会をクリックしたらポップアップを表示
-map.on('click', 'fd42479d8f62b097d0aa(1)', (e) => {
 
-    const properties = e.features[0].properties;
+// ↓ここから追加
+map.on('load', () => {
 
-    new mapboxgl.Popup()
-        .setLngLat(e.lngLat)
-        .setHTML(`
-            <h3>${properties.Name}</h3>
-        `)
-        .addTo(map);
+    // クリック
+    map.on('click', 'fd42479d8f62b097d0aa(1)', (e) => {
 
-});
-// マウスを乗せるとカーソルを変える
-map.on('mouseenter', 'fd42479d8f62b097d0aa(1)', () => {
-    map.getCanvas().style.cursor = 'pointer';
-});
+        const p = e.features[0].properties;
 
-map.on('mouseleave', 'fd42479d8f62b097d0aa(1)', () => {
-    map.getCanvas().style.cursor = '';
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(`
+                <h3>${p.Name}</h3>
+            `)
+            .addTo(map);
+    });
+
+    // カーソル変更
+    map.on('mouseenter', 'fd42479d8f62b097d0aa(1)', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    map.on('mouseleave', 'fd42479d8f62b097d0aa(1)', () => {
+        map.getCanvas().style.cursor = '';
+    });
+
 });
