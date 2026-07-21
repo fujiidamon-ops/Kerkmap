d92234e1005abfbc7489e5bcb6006feb99a1fa66
fujiidamon-ops <fujiidamon@gmail.com>
2026-07-21 20:33:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-new mapboxgl.Popup({
+// あなたのAccess Token
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhY2hhMzI4IiwiYSI6ImNtcWFnYTcyNDA2Z2MycnBwNWJ2Z2pucXkifQ.Z7dIsGQl4CeGw4HkYKh6qg';
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/chacha328/cmr7bti6i004a01rd8c7eb7rk',
+    center: [5.3, 52.1],
+    zoom: 7
+});
+
+map.addControl(new mapboxgl.NavigationControl());
+map.addControl(new mapboxgl.FullscreenControl());
+
+map.addControl(new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true
+}));
+
+// ↓ここから追加
+map.on('load', () => {
+
+    // クリック
+    map.on('click', 'fd42479d8f62b097d0aa(1)', (e) => {
+
+        const p = e.features[0].properties;
+
+        new mapboxgl.Popup({
     closeButton: true,
     closeOnClick: true,
     maxWidth: "320px"
@@ -44,3 +72,15 @@ new mapboxgl.Popup({
 </div>
 `)
 .addTo(map);
+    });
+
+    // カーソル変更
+    map.on('mouseenter', 'fd42479d8f62b097d0aa(1)', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    map.on('mouseleave', 'fd42479d8f62b097d0aa(1)', () => {
+        map.getCanvas().style.cursor = '';
+    });
+
+});
